c139b853ee85b79ad3cf58fa125d339899c74965
hadafresan <77838310+hadafresan@users.noreply.github.com>
2021-01-22 23:37:26 +0330

    contact page first code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Targeted.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
