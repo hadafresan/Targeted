5cc4fab6c695683066e0e8ff4cd2f9154525adfb
hadafresan <77838310+hadafresan@users.noreply.github.com>
2021-03-03 14:07:05 +0330

    design page target 1.1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Targeted.Controllers
         {
             return View();
         }
+        public IActionResult Target()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
